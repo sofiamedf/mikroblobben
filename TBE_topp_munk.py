f7aed60f1b32e48e4d5c16eb97f7dfdd8e5c993a
sofiamedf <70717757+sofiamedf@users.noreply.github.com>
2021-10-25 11:08:22 +0200

    flyttad till rätt mapp

--- TBE_topp_munk.py
+++
@@ -1,9 +0,0 @@
-import plotly.graph_objects as go
-import plotly
-
-labels = ['Stockholm','Uppsala','Västra Götaland','Södermanland', 'Östergötland', 'Övriga']
-values = [41.0, 12.8, 12.3, 9.3, 5.3, 19.3]
-
-# Med "hole" skapar du själva hålet i mitten
-fig = go.Figure(data=[go.Pie(labels=labels, values=values, hole=.3)])
-fig.show()
