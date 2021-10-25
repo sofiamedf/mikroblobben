66421cface28eca3720439c4fda9c5b0155ac960
sofiamedf <70717757+sofiamedf@users.noreply.github.com>
2021-10-25 10:21:31 +0200

    Update TBE_topp_munk.py

--- TBE_topp_munk.py
+++ TBE_topp_munk.py
@@ -7,5 +7,3 @@ values = [41.0, 12.8, 12.3, 9.3, 5.3, 19.3]
 # Med "hole" skapar du själva hålet i mitten
 fig = go.Figure(data=[go.Pie(labels=labels, values=values, hole=.3)])
 fig.show()
-
-plotly.offline.plot(fig, filename="TBE_topp_munk.html")
